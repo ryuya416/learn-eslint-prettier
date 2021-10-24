db10b9cefc12cc73f415a43cd7e9661fa5c06be2
ryuya416 <h2luv0r1a8@icloud.com>
2021-10-24 12:55:02 +0900

    ✨ eslintrcの設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,18 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: "plugin:react/recommended",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 13,
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: [],
+  extends: ["plugin:react/recomended"],
   rules: {
     semi: "error",
     quotes: "error",
+    "no-undef": "error",
     "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
   },
 };
